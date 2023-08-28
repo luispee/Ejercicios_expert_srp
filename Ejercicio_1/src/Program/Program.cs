478cb4800f9b4b20fc14c5d9ef6f53e51959ad62
luispee <lpgaudiano99@gmail.com>
2023-08-28 16:50:40 +0200

    primeros sector, shelf y libros. Falta que el shelf este asociado al sector y los libros a los shelves

--- Ejercicio_1/src/Program/Program.cs
+++ Ejercicio_1/src/Program/Program.cs
@@ -7,11 +7,16 @@ namespace SRP
     {
         static void Main()
         {
+            Sector sector1 = new Sector("PrimerSector");
+            Shelf shelf1 = new Shelf(1);
+            Console.WriteLine(sector1.Name);
+            Console.WriteLine(shelf1.numShelf);
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
-
+            //book1.ShelveBook("A","7");
+            //book2.ShelveBook("B","3");
+            Console.WriteLine(book1.Title);
+            Console.WriteLine(book2.Title);
         }
     }
 }
